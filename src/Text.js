9703a53e30c0544bcab60aa77fe3e00ca2e98792
Cameron <cameron.trando@yahoo.com>
2018-06-10 19:35:50 -0700

    fixing typos

--- src/Text.js
+++ src/Text.js
@@ -1,19 +1,3 @@
-export let introText = `Every passing day, society becomes more and more integrated with social media. New communication technologies such as Facebook, Instagram, and Snapchat have become widespread and common, playing off people's addiction to validation and attention. NEWLINE It's wrong. We need to stop. And I'll show you why.`;
-export const introTitle = `Technology is hurting us`;
-
-export let negTextLevel1 = `In that case, listen to this. In 2017 alone, 94% of Americans owned phones, and looking at Americans aged 17-29, that number rose to 100%. We grow anxious without our technological companions, and on average we use them for over 4 and 1/2 hours per day! NEWLINE Let that sink in. NEWLINE 4 and 1/2 hours. We use our phones and social media daily, and we are spending so much of our precious time, so much of our precious lives, on something so inanimate and replaceable`;
-export let posTextLevel1 = `It's good that you agree, but it's also important to understand why our phones and social media are hurting us. In 2017 alone, 94% of Americans owned phones, and looking at the youth population, that number rose to 100%. We grow anxious without our technological companions, and on average we use them for over 4 and 1/2 hours per day! We use our phones and social media daily, and we are spending so much of our precious time, so much of our precious lives, on something so inanimate and replaceable`;
-export const textLevel1Neutral = `We are addicted to our phones`;
-
-export let posTextLevel2 = 'Overuse of our phones and social media is destructive: a study by University of Virginia researchers Grace Chou and Nicholas Edge found that people who used social media more were unhappier with their lives and believed life was not fair. This implies that all this time spent on our phones and social media can seriously affect how we perceive the world. We cannot underestimate the effects modern communication technology has on our minds.';
-export let negTextLevel2 = `That's fair, so here is some evidence that overusing social media and our phones is destructive. A study by University of Virginia researchers Grace Chou and Nicholas Edge found that people who used social media more were unhappier with their lives and believed life was not fair. NEWLINE This implies that all this time spent on our phones and social media can seriously affect how we perceive the world. We cannot underestimate the effects modern communication technology has on our minds.`;
-export const textLevel2Neutral = 'Our phones depress us, they change us';
-
-export let negTextLevel3 = `If you still don't believe me, know that many others have felt the isolation and suffering that obsessing over social media creates. Sophie Reynolds, a student at UC San Diego, struggled to find her 'group', so she fell into the trap of using social media for attention and relationships. NEWLINE She soon found herself miserable and keeping up a facade, and just like Chou and Edge observed, she was depressed about her life. NEWLINE Even if you have never experienced something like this... NEWLINE Feeling sad when our friends post their happy memories without us... NEWLINE Feeling alone when we try to substitute instant messaging for talking... NEWLINE It's natural, and social media brings all those painful emotions out.`
-export let posTextLevel3 = `It's depressing, really. NEWLINE Our phones have so much control over us, and even though we understand how they can destroy us, we cannot help but use them. NEWLINE It's insidious; we want to connect with others, we want to be in the company of others, and our phones give us an easy drug for our social natures, and we continue to take those pills until we break. NEWLINE But the worst part is, depressing us is not the only thing our phones do; it's scary.`;
-export const textLevel3Pos = 'A self-harming drug';
-export const textLevel3Neg = `It's common: Phones hurt us all`;
-
 export let text0 = {
     title: "Technology is hurting us",
     posChoice: "Tell me more",
@@ -31,7 +15,7 @@ export let text1 = {
     index: 1,
     leftIndex: 3,
     rightIndex: 3,
-    content: "It's good that you agree, but it's also important to understand why phones and social media are hurting society. In 2017 alone, 95% of Americans owned phones, and looking at the youth population (age 18-29), that number rose to 100%. Americans grow anxious without our technological companions, and on average they use them for over 4 and 1/2 hours per day! They use our phones and social media daily, and they are spending so much of our precious time, so much of their precious lives, on something so inanimate and replaceable."
+    content: "It's good that you agree, but it's also important to understand why phones and social media are hurting society. In 2017 alone, 95% of Americans owned phones, and looking at the youth population (age 18-29), that number rose to 100%. Americans grow anxious without their technological companions, and on average they use them for over 4 and 1/2 hours per day! They use their phones and social media daily, and they are spending so much of their precious time, so much of their precious lives, on something so inanimate and replaceable."
 };
 
 export let text2 = {
@@ -51,7 +35,7 @@ export let text4 = {
     index: 4,
     leftIndex: 3,
     rightIndex: 3,
-    content:"Sure, you may not use your phones that much, but I completely believe that society is becoming dependent on these small devices.  Larry Rosen, a Psychology Professor at California State Los Dominguez, analyzed how students aged 13-17 reacted after not having their phones, and astoundingly he found that only after 15 minutes, they began to show anxiety and fear! NEWLINE 15 minutes! NEWLINE Americans rely so much on our phones…for everything! There is no excuse that can be made: they have an addiction. As a society, we have come to the point where not having our phones can cause withdrawal symptoms. NEWLINE It really is like a drug. NEWLINE But there’s more…"
+    content:"Sure, you may not use your phones that much, but I completely believe that society is becoming dependent on these small devices.  Larry Rosen, a Psychology Professor at California State Los Dominguez, analyzed how students aged 13-17 reacted after not having their phones, and astoundingly he found that only after 15 minutes, they began to show anxiety and fear! NEWLINE 15 minutes! NEWLINE Americans rely so much on their phones…for everything! There is no excuse that can be made: they have an addiction. As a society, we have come to the point where not having our phones can cause withdrawal symptoms. NEWLINE It really is like a drug. NEWLINE But there’s more…"
 };
 
 export let text3 = {
@@ -102,7 +86,7 @@ export let text8 = {
     index: 8,
     leftIndex: 7,
     rightIndex: 7,
-    content:"As I mentioned before, on average we spend 4 ½ hours of our day on our phones, but what does that actually mean? If the average U.S adult gets 7 hours of sleep, we have 17 hours of working time. Realistically that goes to 15-16 after meals and other tasks. NEWLINE That means over a quarter of our day is spent on our phones. NEWLINE That turns into over a quarter of our year. NEWLINE Imagine how much we waste in our lifetime… NEWLINE All that precious time, just wasted… NEWLINE Under the popular saying ’10,000 hours makes a master’, think of all the missed opportunities; the missed conversations and relationships; the missed experiences. NEWLINE What are we doing with our lives?"
+    content:"As I mentioned before, on average Americans spend 4 ½ hours of their day on their phones, but what does that actually mean? If the average U.S adult gets 7 hours of sleep, we have 17 hours of working time. Realistically that goes to 15-16 after meals and other tasks. NEWLINE That means over a quarter of the typical Americans' day is spent on their phones. NEWLINE That turns into over a quarter of a year. NEWLINE Imagine how much wasted in a lifetime… NEWLINE All that precious time, just wasted… NEWLINE Under the popular saying ’10,000 hours makes a master’, think of all the missed opportunities; the missed conversations and relationships; the missed experiences. NEWLINE What are Americans doing with their lives?"
 };
 
 export let text9 = {
